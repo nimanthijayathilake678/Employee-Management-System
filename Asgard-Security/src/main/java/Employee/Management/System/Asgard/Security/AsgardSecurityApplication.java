c04ed8a5e853cf1ac2f815f9b34c8d3a5f3c6211
nimanthijayathilake678 <jayathilake.nimanthi99@gmail.com>
2025-06-05 10:32:18 +0530

    Add  .idea/.gitignore

---
+++ Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/AsgardSecurityApplication.java
@@ -0,0 +1,13 @@
+package Employee.Management.System.Asgard.Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsgardSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsgardSecurityApplication.class, args);
+	}
+
+}
---
+++ Asgard-Security/src/test/java/Employee/Management/System/Asgard/Security/AsgardSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package Employee.Management.System.Asgard.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsgardSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
