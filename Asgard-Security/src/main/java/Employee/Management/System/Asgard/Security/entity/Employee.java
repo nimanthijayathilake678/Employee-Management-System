e5e8c4599827acae622872d895e22315d66a0f09
nimanthijayathilake678 <jayathilake.nimanthi99>
2025-06-12 10:46:01 +0530

    Add Entity-Attendence,DutyPoint,PaySheet

--- Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/entity/Employee.java
+++ Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/entity/Employee.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "employee")
+@Table(name = "Employee")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long employee_id;
 
     @Column(name = "employee_code", nullable = false, unique = true)
     private String employeeCode;
