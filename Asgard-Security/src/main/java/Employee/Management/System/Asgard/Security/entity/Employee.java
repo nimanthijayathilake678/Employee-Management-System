9bd0b25afc73c1d22fed4dc0f00dbc60a8e96fcf
nimanthijayathilake678 <jayathilake.nimanthi99>
2025-06-12 09:13:32 +0530

    Add Entity files - Employee

---
+++ Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/entity/Employee.java
@@ -0,0 +1,64 @@
+package Employee.Management.System.Asgard.Security.entity;
+
+import Employee.Management.System.Asgard.Security.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "employee_code", nullable = false, unique = true)
+    private String employeeCode;
+
+    @Column(name = "first_name", nullable = false ,length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false , length = 100)
+    private String lastName;
+
+    @Column(nullable = false,unique = true, length = 100)
+    private String email;
+
+    private String phone;
+    private String position;
+
+    @Column(name = "hire_date", nullable = false)
+    private LocalDateTime hireDate;
+
+    @Column(name = "base_salary", nullable = false)
+    private BigDecimal baseSalary;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_active")
+    private boolean active;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
