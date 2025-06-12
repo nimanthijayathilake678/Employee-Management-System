1dd8d17463f07b86c988b534a604478964891c64
nimanthijayathilake678 <jayathilake.nimanthi99>
2025-06-12 11:40:09 +0530

    Implementation in Service - Employee Service

---
+++ Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/entity/dto/EmployeeDTO.java
@@ -0,0 +1,22 @@
+package Employee.Management.System.Asgard.Security.entity.dto;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmployeeDTO {
+
+    private Long employeeId;
+    private String employeeCode;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String position;
+    private LocalDateTime hireDate;
+    private BigDecimal baseSalary;
+    private boolean active;
+}
