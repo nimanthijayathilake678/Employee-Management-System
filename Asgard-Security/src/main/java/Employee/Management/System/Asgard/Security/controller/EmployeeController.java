d4e3622a99abea3728d454fe8bbdaca6af6cb9c0
nimanthijayathilake678 <jayathilake.nimanthi99>
2025-06-13 20:38:18 +0530

    Implementations - Employee Contoller

---
+++ Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package Employee.Management.System.Asgard.Security.controller;
+
+import Employee.Management.System.Asgard.Security.entity.dto.EmployeeDTO;
+import Employee.Management.System.Asgard.Security.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+    // Create a new employee
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        EmployeeDTO createdEmployee = employeeService.createEmployee(employeeDTO);
+        return ResponseEntity.ok(createdEmployee); // 200
+    }
+}
--- Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/repository/EmployeeRepository.java
+++ Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package Employee.Management.System.Asgard.Security.repository;
 
 import Employee.Management.System.Asgard.Security.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository <Employee ,Long> {
 }
