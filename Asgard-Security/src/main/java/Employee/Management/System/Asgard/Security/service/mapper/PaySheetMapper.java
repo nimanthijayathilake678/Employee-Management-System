71ef1088fd94f37b01df32443ecfc783ed7aef1d
nimanthijayathilake678 <jayathilake.nimanthi99>
2025-06-14 12:34:53 +0530

    IMplementation - PaySheetManagment MapperImplimentation

--- Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/service/mapper/PaySheetMapper.java
+++ Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/service/mapper/PaySheetMapper.java
@@ -1,7 +1,13 @@
 package Employee.Management.System.Asgard.Security.service.mapper;
 
+import Employee.Management.System.Asgard.Security.entity.Employee;
 import Employee.Management.System.Asgard.Security.entity.Paysheet;
+import Employee.Management.System.Asgard.Security.entity.User;
 import Employee.Management.System.Asgard.Security.entity.dto.PaysheetDTO;
+import Employee.Management.System.Asgard.Security.enums.PaysheetStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public class PaySheetMapper {
     public Paysheet toEntity(PaysheetDTO paysheetDTO){
@@ -10,6 +16,16 @@ public class PaySheetMapper {
             return null;
         }
         return Paysheet.builder()
-                .paySheet_id(paysheetDTO)
+                .paySheet_id(paysheetDTO.getPaySheet_id())
+                .employee(paysheetDTO.getEmployee())
+                .payPeriodMonth(paysheetDTO.getPayPeriodMonth())
+                .baseSalary(paysheetDTO.getBaseSalary())
+                .overtimePay(paysheetDTO.getOvertimePay())
+                .bonuses(paysheetDTO.getBonuses())
+                .deductions(paysheetDTO.getDeductions())
+                .grossPay(paysheetDTO.getGrossPay())
+                .taxDeductions(paysheetDTO.getTaxDeductions())
+                .netPay(paysheetDTO.getNetPay())
+                .status(paysheetDTO.getStatus()).build();
     }
 }
