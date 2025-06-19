8c09a4fe3c668b7de876d59ba3695a2ca2b0eaa8
nimanthijayathilake678 <jayathilake.nimanthi99>
2025-06-19 09:41:42 +0530

    Implimentation - DUTYPOINT_MANAGMENT Controller Implimentation

--- Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/controller/DutyPointController.java
+++ Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/controller/DutyPointController.java
@@ -1,4 +1,15 @@
 package Employee.Management.System.Asgard.Security.controller;
 
+import Employee.Management.System.Asgard.Security.service.DutyPointService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/DutyPoint")
 public class DutyPointController {
+    private final DutyPointService dutyPointService;
+
+    public DutyPointController (DutyPointService dutyPointService){
+        this.dutyPointService=dutyPointService;
+    }
 }
