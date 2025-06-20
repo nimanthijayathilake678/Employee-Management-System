90cccf72a29083f58370f8d98400f9188c5bafba
nimanthijayathilake678 <jayathilake.nimanthi99>
2025-06-20 08:54:34 +0530

    Implemetation - Implimentations on Attendance controller and the Attendance Service

---
+++ Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/service/AttendanceService.java
@@ -0,0 +1,8 @@
+package Employee.Management.System.Asgard.Security.service;
+
+import Employee.Management.System.Asgard.Security.entity.dto.AttendanceDTO;
+
+public class AttendanceService {
+    public AttendanceDTO createAttendance(AttendanceDTO attendanceDTO) {
+    }
+}
