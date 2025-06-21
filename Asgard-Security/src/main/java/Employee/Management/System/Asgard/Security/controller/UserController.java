045bd552c2bea61e16e9928549c4d81f208c29a3
nimanthijayathilake678 <jayathilake.nimanthi99>
2025-06-21 10:35:08 +0530

    Implienations -Implimentations in User managemnet

--- Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/controller/UserController.java
+++ Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/controller/UserController.java
@@ -1,4 +1,24 @@
 package Employee.Management.System.Asgard.Security.controller;
 
+import Employee.Management.System.Asgard.Security.entity.dto.UserDTO;
+import Employee.Management.System.Asgard.Security.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Create a new user record
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser (@RequestBody UserDTO userDTO) {
+        UserDTO createdUser  = userService.createUser (userDTO);
+        return ResponseEntity.ok(createdUser );
+    }
 }
--- Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/service/impl/UserServiceImpl.java
+++ Asgard-Security/src/main/java/Employee/Management/System/Asgard/Security/service/impl/UserServiceImpl.java
@@ -1,10 +1,34 @@
 package Employee.Management.System.Asgard.Security.service.impl;
 
 import Employee.Management.System.Asgard.Security.entity.dto.UserDTO;
+import Employee.Management.System.Asgard.Security.entity.User; // Assuming there is a User entity
+import Employee.Management.System.Asgard.Security.repository.UserRepository; // Assuming there is a User repository
 import Employee.Management.System.Asgard.Security.service.UserService;
+import Employee.Management.System.Asgard.Security.service.mapper.UserMapper; // Assuming there is a User mapper
+import Employee.Management.System.Asgard.Security.service.validator.UserValidator; // Assuming there is a User validator
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class UserServiceImpl implements UserService {
-    public UserDTO createuser (UserDTO userDTO){
 
+    @Autowired
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final UserValidator userValidator;
+
+    public UserServiceImpl(UserRepository repository, UserMapper mapper, UserValidator validator) {
+        this.userRepository = repository;
+        this.userMapper = mapper;
+        this.userValidator = validator;
+    }
+
+    @Override
+    public UserDTO createUser (UserDTO userDTO) {
+        userValidator.validateForCreate(userDTO);
+        User entity = userMapper.toEntity(userDTO);
+        return userMapper.toDTO(userRepository.save(entity));
     }
 }
